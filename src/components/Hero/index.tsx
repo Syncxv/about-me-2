57edcd87b1fa77bd55d3942221e8be4595c625a3
Syncxv <aruldevarackal1@gmail.com>
2022-05-20 01:23:32 +0530

    opend state and pointer thingy

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,19 @@
-import { Component } from "solid-js";
+import { Component, createSignal, createEffect } from "solid-js";
 import styles from "./Hero.module.scss";
 const Hero: Component = () => {
+    const [opend, setOpenState] = createSignal(false);
+    createEffect(() => {
+        console.log(opend());
+    });
     return (
-        <div class={styles.hero}>
-            <h1>Aria</h1>
-            <div className="line"></div>
-            <div className={styles.descWrapper}>
-                <p>good day</p>
-                <p>Im a web developer</p>
+        <div className={styles.wrapper}>
+            <div onClick={() => setOpenState(!opend())} class={styles.hero}>
+                <h1>Aria</h1>
+                <div className="line"></div>
+                <div className={styles.descWrapper}>
+                    <p>good day</p>
+                    <p>Im a web developer</p>
+                </div>
             </div>
         </div>
     );

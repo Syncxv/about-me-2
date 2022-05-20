2282d38655c843e1506cb5444e128188464b1fd9
Syncxv <aruldevarackal1@gmail.com>
2022-05-20 05:57:53 +0530

    pfp change and discord button eh

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -1,17 +1,21 @@
 import { Component } from "solid-js";
 import { setOpenState } from "../../App";
 import styles from "./Info.module.scss";
+import PFP from "../../assets/pfp.png";
+import Discord from "../Icons/Discord";
+import copy from "../../util/copy";
 const InfoStuff: Component = () => {
     return (
         <>
             <div className={styles.infoWrapper}>
                 <div className={styles.info}>
-                    <img
-                        class={styles.avatar}
-                        src="https://cdn.discordapp.com/avatars/549244932213309442/5b3984903ff4d507f93465a1e1d86ec7.webp"
-                        alt=""
-                    />
+                    <img class={styles.avatar} src={PFP} alt="" />
                     <h2>Aria</h2>
+                    <div className={styles.contact}>
+                        <span onClick={() => copy("Aria#8171")} className={styles.item}>
+                            <Discord />
+                        </span>
+                    </div>
                 </div>
                 <span onClick={() => setOpenState(false)} className={styles.backButton}>
                     Back

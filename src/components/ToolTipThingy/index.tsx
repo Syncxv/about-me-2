b77af10655e70d62b2d1f4fd9c3667ffb717e29f
Syncxv <aruldevarackal1@gmail.com>
2022-05-22 03:26:26 +0530

    OK made the children take a function instead

--- src/components/ToolTipThingy/index.tsx
+++ src/components/ToolTipThingy/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component, createSignal, JSX } from "solid-js";
 import styles from "./bruh.module.scss";
 interface Props {
     opacity?: number;
@@ -11,16 +11,22 @@ interface Props {
     text: string;
     position: "top" | "bottom" | "left" | "right";
     ref?: HTMLElement;
+    children: (onMouseOver: () => any, onMouseLeave: () => any) => JSX.Element;
 }
 
-const ToolTipThingy: Component<Props> = ({ children, ref, text, position, opacity = 0, onClick = () => {} }) => {
+const ToolTipThingy = ({ ref, text, position, opacity = 0, onClick = () => {}, ...props }: Props) => {
+    const [visible, setVisable] = createSignal(false);
+    const what = props.children(
+        () => setVisable(true),
+        () => setVisable(false)
+    );
     return (
-        <span ref={ref} className={styles.ok} onClick={onClick} style={{ "--opacity": opacity }}>
+        <span ref={ref} className={styles.ok} onClick={onClick} style={{ "--opacity": visible() ? 1 : 0 }}>
             <div className={`${styles.tooltip} ${styles[position]}`}>
                 <div className={styles.text}>{text}</div>
                 <div className={styles.thingy} />
             </div>
-            <div className={styles.content}>{children}</div>
+            <div className={styles.content}>{what}</div>
         </span>
     );
 };

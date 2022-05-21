071e97760ea96991a780fc837809ecfcc5872e50
Syncxv <aruldevarackal1@gmail.com>
2022-05-22 03:37:22 +0530

    TOOLTIP COLORS

--- src/components/ToolTipThingy/index.tsx
+++ src/components/ToolTipThingy/index.tsx
@@ -1,27 +1,29 @@
-import { Component, createSignal, JSX } from "solid-js";
+import { createSignal, JSX } from "solid-js";
 import styles from "./bruh.module.scss";
 interface Props {
-    opacity?: number;
     onClick?: (
         e: MouseEvent & {
             currentTarget: HTMLSpanElement;
             target: Element;
         }
     ) => void;
+    opacity?: number;
+    color?: "black" | "white" | "green" | "red";
     text: string;
     position: "top" | "bottom" | "left" | "right";
     ref?: HTMLElement;
+
     children: (onMouseOver: () => any, onMouseLeave: () => any) => JSX.Element;
 }
 
-const ToolTipThingy = ({ ref, text, position, opacity = 0, onClick = () => {}, ...props }: Props) => {
+const ToolTipThingy = ({ ref, text, position, color = "white", opacity = 0, onClick = () => {}, ...props }: Props) => {
     const [visible, setVisable] = createSignal(false);
     const what = props.children(
         () => setVisable(true),
         () => setVisable(false)
     );
     return (
-        <span ref={ref} className={styles.ok} onClick={onClick} style={{ "--opacity": visible() ? 1 : 0 }}>
+        <span ref={ref} className={`${styles.ok} ${styles[color]}`} onClick={onClick} style={{ "--opacity": visible() ? 1 : 0 }}>
             <div className={`${styles.tooltip} ${styles[position]}`}>
                 <div className={styles.text}>{text}</div>
                 <div className={styles.thingy} />
